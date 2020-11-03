64b4f25e6170ec9283d7628ba6cef7b0850fcfa9
fatemeh mehrafrooz <f.mehrafrooz@bitbucket.iais.co>
2020-11-03 15:01:04 +0330

    page2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Main from './main.js';
+import Details from './itemDetails.js';
 import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter  >
       <Route exact path="/" component={Main}></Route>
+      <Route  path="/Details" component={Details}></Route>
     </BrowserRouter  >
   );
 }
